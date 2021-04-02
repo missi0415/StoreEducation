b14af16f95d765f216307cf63a08551df66a6fec
missi0415 <missi0415@gmail.com>
2021-04-02 16:05:09 +0000

    update追加

--- app/controllers/manager/educations_controller.rb
+++ app/controllers/manager/educations_controller.rb
@@ -2,7 +2,45 @@ class Manager::EducationsController < ApplicationController
   def show
     @member = Member.find(params[:id])
     @educations = @member.educations
+    @education_new = Education.new
   end
   
+  
+  def create
+    @education_new = Education.new(education_params)
+    @education.manager_id = current_manager.id
+    if @education.save
+      redirect_to manager_education_path(education)
+    else
+      render:show
+    end
+  end
+
+  def edit
+    @education = Education.find(params[:id])
+    
+  end
+  
+  def update
+    @education = Education.find(params[:id])
+    if @education.update(education_params)
+      redirect_to manager_education_path(@education)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @education = Education.find(params[:id])
+    @education.destroy
+    redirect_to manager_educations_path
+  end  
+    
+  private
+  
+  def education_params
+    params.require(:education).permit(:member_id,:title,:body,:progress)
+  end
+
   
 end
