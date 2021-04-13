fbd437c215356987e61cbaab98c044389d341c29
missi0415 <missi0415@gmail.com>
2021-04-13 05:06:00 +0000

    resposive 前

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,18 +1,33 @@
-$(document).on('turbolinks:load',function(){
-  $(".list").each(function() {
-    $(this).hover(function(a, b) {
-        $(this)
-          .children("p")
-          .fadeIn();
-      },
-      function() {
-        $(this)
-          .children("p")
-          .fadeOut();
-      }
-    );
-  });
+$(function() {
+$(".list").hover(function() {
+  //ホバーはマウスがあたっているときだけ
+    $(this).children("p").toggleClass("active")
+  //トグルはクラスが付与してあったら外す
 });
+
+});
+
+
+
+
+
+
+
+// $(document).on('turbolinks:load',function(){
+//   $(".list").each(function() {
+//     $(this).hover(function(a, b) {
+//         $(this)
+//           .children("p")
+//           .fadeIn();
+//       },
+//       function() {
+//         $(this)
+//           .children("p")
+//           .fadeOut();
+//       }
+//     );
+//   });
+// });
 $(document).on('turbolinks:load',function(){
   $(".members ").each(function() {
     $(this).hover(function(a, b) {
